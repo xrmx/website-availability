48e1a69c1386d7b34fb3d6aca3e9ad02844248cc
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2021-02-28 13:09:25 +0100

    tests: add basic tests for checker cli

--- wava/checker/cli.py
+++ wava/checker/cli.py
@@ -12,7 +12,7 @@ from wava.checker.requests_checker import check, cleanup
 logger = logging.getLogger()
 
 
-def main():
+def build_argument_parser():
     parser = argparse.ArgumentParser(description="Website availability checker")
     parser.add_argument(
         "--interval",
@@ -32,12 +32,17 @@ def main():
     parser.add_argument("--url", required=True, help="The HTTP url to check")
     parser.add_argument("--verbose", "-v", action="count", default=0)
 
-    args = parser.parse_args()
+    return parser
+
+
+def main(*args):
+    parser = build_argument_parser()
+    args = parser.parse_args(*args)
 
     valid_url = args.url.startswith("http") and validators.url(args.url, public=True)
     if not valid_url:
         logger.error("URL is invalid, only HTTP/HTTPS url are supported")
-        sys.exit(1)
+        return sys.exit(1)
 
     if args.content_match is not None:
         try:
@@ -45,7 +50,7 @@ def main():
             content_re = re.compile(args.content_match)
         except re.error as e:
             logger.error("Invalid --content-match value: {}".format(str))
-            sys.exit(1)
+            return sys.exit(1)
     else:
         content_re = None
 
